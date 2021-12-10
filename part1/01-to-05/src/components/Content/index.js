b45ed2a56cbb29478e8a0d8d7b34f011648ac618
Eduardo Moura Tavares <edumoura1997@gmail.com>
2021-12-10 10:11:27 -0300

    1.1 to 1.5

---
+++ part1/01-to-05/src/components/Content/index.js
@@ -0,0 +1,14 @@
+import Part from "../Part";
+
+const Content = (props) => {
+  const [part1, part2, part3] = [...props.course.parts];
+  return (
+    <div>
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
+    </div>
+  );
+};
+
+export default Content;
---
+++ part1/01-to-05/src/components/Total/index.js
@@ -0,0 +1,10 @@
+const Total = (props) => {
+  const [part1, part2, part3] = [...props.course.parts];
+  return (
+    <p>
+      Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+    </p>
+  );
+};
+
+export default Total;
