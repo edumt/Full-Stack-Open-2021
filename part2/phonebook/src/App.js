cd94e59dd1e452c1b4a30c9f1b3f9bef3d3fec27
Eduardo Moura Tavares <edumoura1997@gmail.com>
2021-12-12 09:50:40 -0300

    2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,20 +4,31 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name:{" "}
+          <input onChange={(e) => setNewName(e.target.value)} value={newName} />
         </div>
-        <div>debug: {newName}</div>
+        {/* <div>debug: {newName}</div> */}
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
     </div>
   );
 };
