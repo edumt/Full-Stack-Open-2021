4d239fcfafbabfb2542aa7d8e4eee04e94bd5113
Eduardo Moura Tavares <edumoura1997@gmail.com>
2021-12-13 22:36:23 -0300

    2.18

--- part2/phonebook/src/components/PersonForm/index.js
+++ part2/phonebook/src/components/PersonForm/index.js
@@ -8,12 +8,27 @@ const PersonForm = ({ persons, setPersons }) => {
   const handleSubmit = (e) => {
     const newPerson = { name: newName, number: newNumber };
     e.preventDefault();
-    if (persons.some((person) => person.name === newName))
-      alert(`${newName} is already added to phonebook`);
-    else
+
+    if (persons.some((person) => person.name === newName)) {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        const id = persons.find((person) => person.name === newName).id;
+        personService
+          .update(id, newPerson)
+          .then((updatedPerson) =>
+            setPersons([
+              ...persons.filter((person) => person.id !== id),
+              updatedPerson,
+            ])
+          );
+      }
+    } else
       personService
         .create(newPerson)
-        .then((person) => setPersons([...persons, person]));
+        .then((createdPerson) => setPersons([...persons, createdPerson]));
     setNewName("");
     setNewNumber("");
   };
