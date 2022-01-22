b57d8b7bb5e9fc873ad693bacd505b8eb3d8b230
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-01-22 12:34:10 -0300

    4.2: blog model refactor

--- part4/index.js
+++ part4/index.js
@@ -5,15 +5,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const config = require("./utils/config");
 const mongoose = require("mongoose");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blog");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
