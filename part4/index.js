8e1362e31d4f057f9b305c20acedc0bd7e9c58f1
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-01-22 12:28:26 -0300

    4.2: env variables refactor

--- part4/index.js
+++ part4/index.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
 //const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const config = require("./utils/config");
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
@@ -15,7 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
@@ -44,7 +44,7 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = config.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
