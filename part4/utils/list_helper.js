7a827ace97c9059095a2472c72fea527fbcf23db
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-01-22 14:24:00 -0300

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,7 +4,15 @@ const dummy = () => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  const likesArray = blogs.map((blog) => blog.likes);
+  const favoriteIndex = likesArray.indexOf(Math.max(...likesArray));
+
+  return blogs[favoriteIndex];
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
