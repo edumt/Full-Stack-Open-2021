1897853ec7dea2efc6d318ceef72be866c48647d
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-01-22 12:14:34 -0300

    4.1

---
+++ part4/index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+//const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+
+morgan.token("sent-data", function (req) {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :sent-data"
+  )
+);
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
