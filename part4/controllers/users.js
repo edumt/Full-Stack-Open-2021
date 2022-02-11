9ac4a2187dd5a8d3874412635ceac9fbd3e6ce6a
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-02-11 13:01:09 -0300

    4.15

---
+++ part4/controllers/users.js
@@ -0,0 +1,41 @@
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+usersRouter.post("/", async (request, response) => {
+  const body = request.body;
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
+});
+
+// usersRouter.put("/:id", async (request, response) => {
+//   const user = request.body;
+
+//   const updatedUser = await User.findByIdAndUpdate(request.params.id, user, {
+//     runValidators: true,
+//     new: true,
+//   });
+
+//   response.json(updatedUser);
+// });
+
+// usersRouter.delete("/:id", async (request, response) => {
+//   await User.findByIdAndDelete(request.params.id);
+//   response.status(204).end();
+// });
+
+module.exports = usersRouter;
