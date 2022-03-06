950dde013579e287395c4c66e8da811721f606e9
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-03-06 14:06:29 -0300

    5.1

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,24 +1,25 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blogs from "./components/Blogs";
+import Login from "./components/Login";
+import blogService from "./services/blogs";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {user === null ? (
+        <Login setUser={setUser} />
+      ) : (
+        <Blogs blogs={blogs} user={user} />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ part5/src/components/Blogs.js
@@ -0,0 +1,15 @@
+import Blog from "./Blog";
+
+const Blogs = ({ blogs, user }) => {
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged-in</p>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default Blogs;
