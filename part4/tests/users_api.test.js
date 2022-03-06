d4422908ff2dbc39df59d4d5a39e276ef7dcf9cd
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-03-06 09:57:42 -0300

    4.16

--- part4/tests/users_api.test.js
+++ part4/tests/users_api.test.js
@@ -7,12 +7,13 @@ const api = supertest(app);
 // const User = require("../models/user");
 
 describe("when creating a new user", () => {
-  throw { name: "NotImplementedError", message: "too lazy to implement" };
+  test("invalid add user operation returns a suitable status code and error message", async () => {
+    const invalidUser = { name: "asdf", username: "as", password: "df" };
+    await api.post("/api/users").send(invalidUser).expect(400);
+  });
+  // throw { name: "NotImplementedError", message: "too lazy to implement" };
+  // should implement more rigorous testing
   // test("invalid users are not created", async () => {});
-  // test("invalid add user operation returns a suitable status code and error message", async () => {
-  //   const invalidUser = { name: "asdf", username: "as", password: "df" };
-  //   await api.post("/api/users").send(invalidUser).expect(400);
-  // });
 });
 
 afterAll(() => {
