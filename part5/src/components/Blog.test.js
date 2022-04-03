a625f57eb68e4f4b09c8e6cd8df0687f236cc3d1
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-04-03 14:02:14 -0300

    5.14

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
-test("renders content", () => {
-  const blog = {
-    title: "Component testing is done with react-testing-library",
-    author: "Jest",
-    url: "localhost",
-    likes: 420,
-  };
+const blog = {
+  title: "Component testing is done with react-testing-library",
+  author: "Jest",
+  url: "localhost",
+  likes: 420,
+  user: { name: "asdf", username: "asdf" },
+};
 
+const user = { username: "asdf" };
+
+test("renders with basic content", () => {
   render(<Blog blog={blog} />);
 
   screen.getByText(`${blog.title} | ${blog.author}`);
@@ -18,4 +22,13 @@ test("renders content", () => {
   expect(url).toBeNull();
   const likes = screen.queryByText(blog.likes);
   expect(likes).toBeNull();
+});
+
+test("expands content when view button is clicked", () => {
+  render(<Blog blog={blog} user={user} />);
+
+  const button = screen.getByText("view");
+  userEvent.click(button);
+  screen.getByText(blog.url);
+  screen.getByText(`likes ${blog.likes}`);
 });
