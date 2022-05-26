f505e38a46f7494c512070fd3c53ceafbcd3c0e5
Eduardo Moura Tavares <eduardo.tavares@4mti.com.br>
2022-05-26 15:24:53 -0300

    7.5

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -97,6 +97,12 @@ const CreateNew = (props) => {
     });
   };
 
+  const handleReset = () => {
+    content.reset();
+    author.reset();
+    info.reset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -114,6 +120,9 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset} type="reset">
+          reset
+        </button>
       </form>
     </div>
   );
