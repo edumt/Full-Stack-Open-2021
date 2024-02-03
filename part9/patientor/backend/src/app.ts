a8b16113b17615951d6556743b54d1739ad4e25d
edumt <etavares@callix.com.br>
2024-02-03 19:05:34 -0300

    9.12

--- part9/patientor/backend/src/app.ts
+++ part9/patientor/backend/src/app.ts
@@ -7,6 +7,7 @@ import pingRouter from "./controllers/ping";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.use("/api/ping", pingRouter);
 app.use("/api/diagnoses", diagnosesRouter);
--- part9/patientor/backend/src/controllers/patients.ts
+++ part9/patientor/backend/src/controllers/patients.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import crypto from "node:crypto";
 import patients, { Patient } from "../models/patient";
 
 const patientsRouter = Router();
@@ -15,6 +16,21 @@ patientsRouter.get("/", (_, res) => {
   );
 
   res.json(patientsWithoutSsn);
+});
+
+patientsRouter.post("/", (req, res) => {
+  const body = req.body as Omit<Patient, "id">; // unsafe type cast
+  const patient: Patient = {
+    id: crypto.randomUUID(),
+    name: body.name,
+    occupation: body.occupation,
+    ssn: body.ssn,
+    dateOfBirth: body.dateOfBirth,
+    gender: body.gender,
+  };
+
+  patients.push(patient);
+  res.json(req.body);
 });
 
 export default patientsRouter;
